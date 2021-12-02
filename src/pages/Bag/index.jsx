3d1c4355741d12fa4b579dede295c3acfd4ce966
herculesoliveir <herculesamericano@gmail.com>
2021-12-01 21:51:38 -0300

    .

--- src/pages/Bag/index.jsx
+++ src/pages/Bag/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '../../components/Button/index'
 
 function Bag() {
-  return <Button>SEGUIR PARA O PAGAMENTO</Button>
+  return <Button>SEGUIR PARA O PAGAMENTO..</Button>
 }
 
 export default Bag
